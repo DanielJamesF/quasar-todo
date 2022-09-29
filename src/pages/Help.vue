5895ec7847d65d7e632045ce9773fc790b9cdaf4
DanielJamesF <fredericksd17@gmail.com>
2022-09-29 15:20:41 +0200

    first commit

---
+++ src/pages/Help.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem at voluptatum dicta nulla corrupti delectus dolor quis, explicabo expedita assumenda obcaecati omnis, quod nesciunt, aliquam fuga repudiandae fugit. Optio, explicabo?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem at voluptatum dicta nulla corrupti delectus dolor quis, explicabo expedita assumenda obcaecati omnis, quod nesciunt, aliquam fuga repudiandae fugit. Optio, explicabo?</p>
+  </q-page>
+</template>
+
+<script>
+
+export default ({
+})
+</script>
